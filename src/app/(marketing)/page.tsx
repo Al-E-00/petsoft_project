238c19dc3ee20603f37e0ec551079a8c12db590d
Alessio <alessio.mazzella@siren.io>
2024-03-04 20:43:29 +0100

    Add new files and update configurations

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,3 +1,30 @@
+import Logo from '@/components/logo';
+import Image from 'next/image';
+
 export default function Home() {
-  return <main>Home page</main>;
+  return (
+    <main className='bg-[#5DC9A8] min-h-screen flex items-center justify-center gap-10 xl:flex-row flex-col'>
+      <Image
+        src='https://bytegrad.com/course-assets/react-nextjs/petsoft-preview.png'
+        alt='Preview of PetSoft'
+        height={519}
+        width={472}
+      />
+
+      <div>
+        <Logo />
+        <h1 className='text-5xl font-semibold my-6 max-w-[31.25rem]'>
+          Manage your <span className='font-extrabold'>pet daycare</span> with ease
+        </h1>
+        <p className='text-2xl font-medium max-w-[37.5rem]'>
+          Use PetSoft to easily keep track of pets under your care. Get lifetime
+          access for $299.
+        </p>
+      </div>
+
+      <div className='mt-10'>
+
+      </div>
+    </main>
+  );
 }
