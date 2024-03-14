ce33d2d1600ec28c7c28add49d2b0b94b07bd1c7
Alessio <alessio.mazzella@siren.io>
2024-03-14 20:53:02 +0100

    Dummy list element for the pet-list component

--- src/components/pet-list.tsx
+++ src/components/pet-list.tsx
@@ -1,8 +1,23 @@
+import Image from 'next/image';
+
 export default function PetList() {
   return (
-    <ul className="border-b border-black/[0.08] bg-white">
+    <ul className="gap-3 border-b border-black/[0.08] bg-white">
       <li>
-        <button>Benjamin</button>
+        <button
+          className="flex h-[4.375rem] w-full
+          cursor-pointer items-center gap-3 px-5
+          text-base transition hover:bg-[#EFF1F2] focus:bg-[#EFF1F2]"
+        >
+          <Image
+            src="https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png"
+            alt="Pet image"
+            width={45}
+            height={45}
+            className="rounded-full object-cover"
+          />
+          <p className="font-semibold">Benjamin</p>
+        </button>
       </li>
     </ul>
   );
