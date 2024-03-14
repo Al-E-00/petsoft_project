786f7def592d90a737eef43b158d0337ca5e0236
Alessio <alessio.mazzella@siren.io>
2024-03-14 20:35:42 +0100

    Add new components and update dashboard page layout

--- src/app/(app)/app/dashboard/page.tsx
+++ src/app/(app)/app/dashboard/page.tsx
@@ -1,4 +1,8 @@
 import Branding from '@/components/branding';
+import ContentBlock from '@/components/content-block';
+import PetDetails from '@/components/pet-details';
+import PetList from '@/components/pet-list';
+import SearchForm from '@/components/search-form';
 import Stats from '@/components/stats';
 
 export default function DashboardPage() {
@@ -8,6 +12,24 @@ export default function DashboardPage() {
         <Branding />
 
         <Stats />
+      </div>
+
+      <div className='grid grid-cols-3 grid-rows-[2.8125rem_1fr] gap-4 h-[37.5rem]'>
+        <div className='row-start-1 row-span-1 col-start-1 col-span-1'>
+          <SearchForm />
+        </div>
+
+        <div className='row-start-2 row-span-full col-start-1 col-span-1'>
+          <ContentBlock>
+            <PetList />
+          </ContentBlock>
+        </div>
+
+        <div className='row-start-1 row-span-full col-start-2 col-span-full'>
+          <ContentBlock>
+            <PetDetails />
+          </ContentBlock>
+        </div>
       </div>
     </main>
   );
