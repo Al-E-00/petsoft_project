bd82957df2fa46d808858e4ebfd81b59d7158d67
Alessio <alessio.mazzella@siren.io>
2024-03-16 16:05:29 +0100

    Refactor addPet function and PetForm component

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -2,7 +2,15 @@
 
 import prisma from '@/lib/db';
 
-export async function addPet(pet) {
+export async function addPet(formData) {
+  const pet = {
+    name: formData.get('name'),
+    ownerName: formData.get('ownerName'),
+    imageUrl: formData.get('imageUrl'),
+    age: parseInt(formData.get('age')),
+    notes: formData.get('notes'),
+  };
+
   await prisma.pet.create({
     data: pet,
   });
