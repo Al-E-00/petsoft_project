87a7189b879134fe218c4e1d1e820fac78d2c676
Alessio <alessio.mazzella@siren.io>
2024-03-16 11:21:42 +0100

    Add PetButton component and update DashboardPage and PetDetails

---
+++ src/components/pet-button.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { PlusIcon } from '@radix-ui/react-icons';
+import { Button } from './ui/button';
+
+type PetButtonProps = {
+  actionType: 'add' | 'edit' | 'checkout';
+  children?: React.ReactNode;
+};
+
+export default function PetButton({ actionType, children }: PetButtonProps) {
+  switch (actionType) {
+    case 'add':
+      return (
+        <Button size="icon">
+          <PlusIcon className="h-6 w-6" />
+        </Button>
+      );
+    case 'edit':
+      return <Button variant="secondary">{children}</Button>;
+    case 'checkout':
+      return <Button variant="secondary">{children}</Button>;
+  }
+}
--- src/components/pet-details.tsx
+++ src/components/pet-details.tsx
@@ -3,6 +3,7 @@
 import { usePetContext } from '@/lib/hooks';
 import { Pet } from '@/lib/types';
 import Image from 'next/image';
+import PetButton from './pet-button';
 
 export default function PetDetails() {
   const { selectedPet } = usePetContext();
@@ -36,9 +37,14 @@ type Props = {
 
 function TopBar({ pet }: Props) {
   return (
-    <div className="border-light flex items-center border-b bg-white px-8 py-5">
+    <div className="flex items-center border-b border-light bg-white px-8 py-5">
       <Image src={pet?.imageUrl} alt="Selected pet image" height={75} width={75} className="h-[75px] w-[75px] rounded-full object-cover" />
       <h2 className="ml-5 text-3xl font-semibold leading-7">{pet?.name}</h2>
+
+      <div className="ml-auto space-x-2">
+        <PetButton actionType="edit">Edit</PetButton>
+        <PetButton actionType="checkout">Checkout</PetButton>
+      </div>
     </div>
   );
 }
@@ -59,5 +65,5 @@ function OtherInfo({ pet }: Props) {
 }
 
 function Notes({ pet }: Props) {
-  return <section className="border-light mx-8 mb-9 flex-1 rounded-md border  bg-white px-7 py-5">{pet?.notes}</section>;
+  return <section className="mx-8 mb-9 flex-1 rounded-md border border-light  bg-white px-7 py-5">{pet?.notes}</section>;
 }
