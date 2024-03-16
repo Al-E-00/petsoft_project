d9430a355285db3169c07e3ced2cfb63046ac589
Alessio <alessio.mazzella@siren.io>
2024-03-16 11:04:27 +0100

    Refactor account page and related components

--- src/components/content-block.tsx
+++ src/components/content-block.tsx
@@ -1,3 +1,10 @@
-export default function ContentBlock({ children }: { children: React.ReactNode }) {
-  return <div className="h-full w-full overflow-hidden rounded-sm bg-[#F7F8FA] shadow-sm">{children}</div>;
+import { cn } from '@/lib/utils';
+
+type ContentBlockProps = {
+  children: React.ReactNode;
+  className?: string;
+};
+
+export default function ContentBlock({ children, className }: ContentBlockProps) {
+  return <div className={cn('h-full w-full overflow-hidden rounded-sm bg-[#F7F8FA] shadow-sm', className)}>{children}</div>;
 }
